3547b6387ce949702643bd6567ae65992ecc765b
Harm Berntsen <harm.berntsen@nedap.com>
2019-02-20 18:08:18 +0100

    Remove crt-static from target feature

--- paho-mqtt-sys/build.rs
+++ paho-mqtt-sys/build.rs
@@ -147,6 +147,16 @@ mod build {
         // Use cmake to build the C lib
         let ssl = if cfg!(feature = "ssl") { "on" } else { "off" };
 
+        // Causes https://docs.rs/crate/cc/1.0.17/source/src/lib.rs to add the -static flag which breaks the build
+        // We already ensured that we make a static build via 'PAHO_BUILD_STATIC"
+        {
+            let mut features = env::var("CARGO_CFG_TARGET_FEATURE").unwrap_or(String::new());
+            if features.contains("crt-static") {
+                features = features.replace("crt-static", "");
+                env::set_var("CARGO_CFG_TARGET_FEATURE", features);
+            }
+        }
+
         let mut cmkc = cmake::Config::new("paho.mqtt.c/");
         cmkc
             .define("PAHO_BUILD_STATIC", "on")
